aee276f7b7d3f392c226fc90ac9b122e7e39497b
DBozhkovV <73282224+DBozhkovV@users.noreply.github.com>
2023-04-11 22:16:29 +0300

    Add session based authentication

--- src/main/java/miniyoutube/com/usermanagementservice/dtos/RegisterDTO.java
+++ src/main/java/miniyoutube/com/usermanagementservice/dtos/RegisterDTO.java
@@ -1,8 +1,15 @@
 package miniyoutube.com.usermanagementservice.dtos;
 
-public class UserDTO {
+public class RegisterDTO {
     private String username;
+    private String email;
     private String password;
+
+    public RegisterDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
@@ -10,6 +17,14 @@ public class UserDTO {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
